7ad9f08da83a92502d7da87044eed336f9719b65
Raymundo Mateos <rayma9829@gmail.com>
2025-08-15 16:47:26 -0600

    add: app skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -15,23 +15,73 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _index = 0;
+
+  void setIndex(value) {
+    setState(() {
+      _index = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    String title = _index == 0 ? 'Stopwatch' : 'Timer';
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        onTap: setIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.watch_rounded),
+            label: 'Stopwatch',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.timer), label: 'Timer'),
+        ],
+      ),
+      body: _index == 0 ? MyStopWatch() : MyTimer(),
+    );
+  }
+}
+
+class MyStopWatch extends StatefulWidget {
+  const MyStopWatch({super.key});
+
+  @override
+  State<MyStopWatch> createState() => _MyStopWatchState();
+}
+
+class _MyStopWatchState extends State<MyStopWatch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class MyTimer extends StatefulWidget {
+  const MyTimer({super.key});
+
+  @override
+  State<MyTimer> createState() => _MyTimerState();
+}
+
+class _MyTimerState extends State<MyTimer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
