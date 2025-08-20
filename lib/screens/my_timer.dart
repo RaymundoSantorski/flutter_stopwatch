f683d0e18b862f190d4ab48d93f09db1e5afb197
Raymundo Mateos <rayma9829@gmail.com>
2025-08-19 21:23:54 -0600

    add: validation to update state only if widget is mounted

--- lib/screens/my_timer.dart
+++ lib/screens/my_timer.dart
@@ -1,52 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:stepwatch_practice/services/timer_service.dart';
 import 'package:stepwatch_practice/widgets/timer/timer_display.dart';
 import 'package:stepwatch_practice/widgets/timer/timer_picker.dart';
 
-class MyTimer extends StatefulWidget {
+class MyTimer extends TimerService {
   const MyTimer({super.key});
 
   @override
   State<MyTimer> createState() => _MyTimerState();
 }
 
-class _MyTimerState extends State<MyTimer> {
-  Duration duration = Duration(minutes: 0, seconds: 0);
-  bool running = false;
-
-  Future<void> timerFunction() async {
-    await Future.delayed(Duration(seconds: 1));
-    setState(() {
-      duration = Duration(seconds: duration.inSeconds - 1);
-    });
-    if (duration.inSeconds >= 0) {
-      timerFunction();
-    } else {
-      exitTimer();
-    }
-  }
-
-  void startTimer() {
-    if (duration.inSeconds == 0) return;
-    setState(() {
-      running = true;
-    });
-    timerFunction();
-  }
-
-  void exitTimer() {
-    setState(() {
-      duration = Duration();
-      running = false;
-    });
-  }
-
-  void setDuration(Duration value) {
-    setState(() {
-      duration = value;
-    });
-  }
-
+class _MyTimerState extends TimerServiceState<MyTimer> {
   @override
   Widget build(BuildContext context) {
     return running
---
+++ lib/services/timer_service.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class TimerService extends StatefulWidget {
+  const TimerService({super.key});
+
+  @override
+  State<TimerService> createState() => TimerServiceState();
+}
+
+class TimerServiceState<T extends TimerService> extends State<T> {
+  Duration duration = Duration(minutes: 0, seconds: 0);
+  bool running = false;
+
+  Future<void> timerFunction() async {
+    await Future.delayed(Duration(seconds: 1));
+    if (mounted) {
+      setState(() {
+        duration = Duration(seconds: duration.inSeconds - 1);
+      });
+    }
+    if (duration.inSeconds >= 0) {
+      timerFunction();
+    } else {
+      exitTimer();
+    }
+  }
+
+  void startTimer() {
+    if (duration.inSeconds == 0) return;
+    if (mounted) {
+      setState(() {
+        running = true;
+      });
+    }
+    timerFunction();
+  }
+
+  void exitTimer() {
+    if (mounted) {
+      setState(() {
+        duration = Duration();
+        running = false;
+      });
+    }
+  }
+
+  void setDuration(Duration value) {
+    if (mounted) {
+      setState(() {
+        duration = value;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
